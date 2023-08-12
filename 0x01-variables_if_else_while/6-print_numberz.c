84b3c8363da6e4cf0536cbc5e5cabd91b5548b97
musteemd-93 <muhammedmustapha37@gmail.com>
2023-08-12 14:55:20 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Printing single digit numbers starting from 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
