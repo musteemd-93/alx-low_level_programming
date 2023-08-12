01c54e0450ded0bca3628a1fecd74f42f3b31a10
musteemd-93 <muhammedmustapha37@gmail.com>
2023-08-12 15:25:36 -0700

    base sixteen

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Print all the numbers of base 16 in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		putchar(a);
+	}
+	for (a = 97; a < 103; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
