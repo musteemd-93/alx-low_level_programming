823399f96ba5920c8e8b0711058cc597bfd7eeeb
musteemd-93 <muhammedmustapha37@gmail.com>
2023-08-12 14:06:59 -0700

    single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Getting single digits from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	putchar('\n');
+	return (0);
+}
