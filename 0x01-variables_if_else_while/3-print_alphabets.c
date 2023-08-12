4bd205dceea9f1bdf0996576a87ac17223a93907
musteemd-93 <muhammedmustapha37@gmail.com>
2023-08-12 12:54:28 -0700

    mixed cases

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints lowercase and uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alph[i]);
+	}
+	putchar('\n');
+	return (0);
+}
