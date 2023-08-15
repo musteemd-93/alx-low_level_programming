93a43da35591a672c757fe286d9ec10165e84e35
musteemd-93 <muhammedmustapha37@gmail.com>
2023-08-15 16:16:11 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,10 +2,10 @@
 
 /**
  * _isalpha - checks for alphabetic character
- * @y: the character to be checked
+ * @c: the character to be checked
  * Return: 1 if y is a letter, 0 otherwise
  */
-int _isalpha(int y)
+int _isalpha(int c)
 {
-	return ((y >= 'a' && y <= 'z'));
+	return ((c >= 'a' && c <= 'z'));
 }
