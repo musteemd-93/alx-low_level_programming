357702f6bb2803a2167d4004bedfbd723e19b815
musteemd-93 <muhammedmustapha37@gmail.com>
2023-08-23 16:07:56 -0700

    update

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_number - Print numbers
+ * print_number - Print numbers chars
  * @n: Integer params
  *
  * Return: 0
@@ -23,5 +23,5 @@ void print_number(int n)
 	{
 		print_number(n1 / 10);
 	}
-	putchar((n1 % 10) + 0);
+	putchar((n1 % 10) + '0');
 }
