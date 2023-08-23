af8029f832330c58a232609118bb1678820ebcc7
musteemd-93 <muhammedmustapha37@gmail.com>
2023-08-23 15:35:35 -0700

    update

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,7 +16,7 @@ char *cap_string(char *str)
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
 
-		if (str[index - 1] == ' '||
+		if (str[index - 1] == ' ' ||
 			str[index - 1] == '\t' ||
 			str[index - 1] == '\n' ||
 			str[index - 1] == ',' ||
@@ -24,14 +24,13 @@ char *cap_string(char *str)
 			str[index - 1] == '.' ||
 			str[index - 1] == '!' ||
 			str[index - 1] == '?' ||
-			str[index - 1] == ' '||
+			str[index - 1] == ' ' ||
 			str[index - 1] == '(' ||
 			str[index - 1] == ')' ||
 			str[index - 1] == '{' ||
 			str[index - 1] == '}' ||
 			index == 0)
 			str[index] -= 32;
-		
 		index++;
 	}
 	return (str);
