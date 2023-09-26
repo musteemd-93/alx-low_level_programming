94cc050d1e9de1bb1a242010a148396b28dc9108
musteemd-93 <muhammedmustapha37@gmail.com>
2023-09-26 15:05:55 -0700

    update

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -6,7 +6,7 @@
  * @index: The index to return
  * Return: Pointer to node, or NULL
  */
-listint_t 8get_nodeint_at_index(listint_t *head, unsigned int index)
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 	listint_t *temp = head;
