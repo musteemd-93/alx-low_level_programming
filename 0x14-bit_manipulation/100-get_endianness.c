95040f95663a6377c0755dd5f5f9e80d9c332d50
musteemd-93 <muhammedmustapha37@gmail.com>
2023-09-28 16:38:07 -0700

    correction

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * get_endiannes - Check if machine is big or little
+ * get_endianness - Check if machine is big or little
  * Return: 1 for little or 0 for big
  */
 int get_endianness(void)
 {
 	unsigned int i = 1;
 	char *c = (char *) &i;
+
 	return (*c);
 }
